b73573ff5c56f52b6f9c81985ce89b5b7eb67342
Maurizio Martignano <Maurizio.Martignano@spazioit.com>
2022-10-07 07:53:32 +0200

    Java 19 Support

--- src/main/java/com/spazioit/safacilitator/SAFacilitator.java
+++ src/main/java/com/spazioit/safacilitator/SAFacilitator.java
@@ -47,7 +47,7 @@ public class SAFacilitator {
     private Project currentProject = null;
     private static SAFacilitator myself = null;
     private static boolean guiEnabled = true;
-    private static String version = "1.8";
+    private static String version = "1.9";
     private static int MAX_LINES = 24576;
     private static String fileName = "";
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
